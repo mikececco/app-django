a5204b24e73379fa20704e85f26a7b8664d32aaa
Mike Cecconello <mikececco2000@gmail.com>
2024-01-21 01:20:31 +0100

    Added contact page

--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html') #automatically searches for a templates folder
+
+def contact(request):
+    return render(request, 'core/contact.html')
